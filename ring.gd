76867740acb5cf6ff16c07ecccac8e294101c6af
Daniel <mr_schliesing@hotmail.com>
2024-12-04 20:31:41 -0900

    trying to get rings to work
    also tested some stuff in the flight script

--- ring.gd
+++ ring.gd
@@ -3,11 +3,7 @@ extends Node3D
 func _ready():
 	# Connect the Area3D's body_entered signal to this script
 	$Area3D.body_entered.connect(_on_body_entered)
-	$Area3D.area_entered.connect(_on_area_3d_area_entered)
 
 func _on_body_entered(body: Node) -> void:
-	pass
-
-func _on_area_3d_area_entered(area):
 	GameController.update_score()
 	queue_free()
